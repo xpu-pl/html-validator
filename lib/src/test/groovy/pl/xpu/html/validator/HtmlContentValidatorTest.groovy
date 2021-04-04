3c6775c68d94c989ff0c4d5c85b41fedb2fc4855
Michal Szulc <michal@xpu.pl>
2021-04-04 14:19:01 +0200

    Extended tests (eg for invalid HTML content)

--- lib/src/test/groovy/pl/xpu/html/validator/HtmlContentValidatorTest.groovy
+++ lib/src/test/groovy/pl/xpu/html/validator/HtmlContentValidatorTest.groovy
@@ -5,9 +5,29 @@ import spock.lang.Unroll
 
 class HtmlContentValidatorTest extends Specification {
 
-	void "validateHtmlContent: returns string of possible errors"() {
+	void "validateHtmlContent: returns JSON withhout any errors"() {
 		expect:
 			new HtmlContentValidator().validateHtmlContent("lol") == [messages: []]
+	}
+
+	void "validateHtmlContent: returns JSON with an errors"() {
+		when: "we send not closed span element and not opened dic element"
+			def result = new HtmlContentValidator().validateHtmlContent("<span>lol</div>")
+		then:
+			result.messages.size() == 3
+			result.messages.type == ['error', 'error', 'error']
+			result.messages[0] == [
+					type       : 'error', lastLine: 1, lastColumn: 81, firstColumn: 76,
+					message    : 'Stray end tag “div”.', extract: '><span>lol</div></body',
+					hiliteStart: 10, hiliteLength: 6]
+			result.messages[1] == [
+					type       : 'error', lastLine: 1, lastColumn: 88, firstColumn: 82,
+					message    : 'End tag for  “body” seen, but there were unclosed elements.', extract: '>lol</div></body></html',
+					hiliteStart: 10, hiliteLength: 7]
+			result.messages[2] == [
+					type       : 'error', lastLine: 1, lastColumn: 72, firstColumn: 67,
+					message    : 'Unclosed element “span”.', extract: 'ead><body><span>lol</d',
+					hiliteStart: 10, hiliteLength: 6]
 	}
 
 	@Unroll
